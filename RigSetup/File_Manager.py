223fae71379d7a1843126b306dc55c9c795db8ea
Trevor Payne <crashandexplode@hotmail.com>
2020-06-28 18:11:32 -0500

    Completion of first pass for File Manager

--- RigSetup/File_Manager.py
+++ RigSetup/File_Manager.py
@@ -7,11 +7,20 @@ class File_Manager():
         self._templatePath = templateFolderPath
         self._buildFolderPath = ''
         self._customTemplatePath = ''
-        self._buildPaths = {}
+        self._meshPath = ''
+        self._buildFileData = {}
         self._nextBuildVersion = {}
+        self._buildIndex = {}
+
         self._skeletonTemplates = {}
         self._skeletonCustomTemplates = {}
+        self._behaviorTemplates = {}
+        self._behaviorCustomTemplates = {}
+        self._appearanceTemplates = {}
+        self._appearanceCustomTemplates = {}
+        self._skinningTemplates = {}
 
+#========== ACCESSORS + MUTATORS ==================================
     def SetCustomTemplatePath(self, path):
         self._customTemplatePath = path
     
@@ -24,10 +33,17 @@ class File_Manager():
     def GetBuildFolderPath(self):
         return self._buildFolderPath
 
-    def GetBuildFilesInfo(self):
+    def SetMeshPath(self, path):
+        self._meshPath = path
+
+    def GetMeshPath(self):
+        return self._meshPath
+
+    def InitBuildFilesInfo(self):
         '''For loading and saving build files for each part'''
-        self._buildPaths.clear()
+        self._buildFileData.clear()
         self._nextBuildVersion.clear()
+        self._buildIndex.clear()
 
         skelPath = os.path.join(self._buildFolderPath, 'Skeleton')
         bhvPath = os.path.join(self._buildFolderPath, 'Behaviors')
@@ -39,8 +55,8 @@ class File_Manager():
                 'Appearance': appPath,
                 'Skinning': skinPath}
         for k,v in temp.items():
-            self._buildPaths[k] = []
-            self._nextBuildVersion[k] = -2
+            self._buildFileData[k] = []
+            self._nextBuildVersion[k] = 0
             if not os.path.isdir(v):
                 os.mkdir(v)
             for f in os.listdir(v):
@@ -51,10 +67,11 @@ class File_Manager():
                 self._nextBuildVersion[k] = max(self._nextBuildVersion[k], data.version)
                 data.path = os.path.join(v, f)
                 data.date = time.ctime(os.path.getmtime(data.path))
-                self._buildPaths[k].append(data)
+                self._buildFileData[k].append(data)
             self._nextBuildVersion[k] += 1
+            self._buildIndex[k] = len(self._buildFileData[k])-1
 
-#========== SKELETONS ==================================
+#========== SKELETON ==================================
 
     def GetSkeletonTemplates(self):
         # Files NEED to exist
@@ -64,6 +81,7 @@ class File_Manager():
             filePath = os.path.join(skelPath, f)
             fileName, ext = os.path.splitext(f)
             self._skeletonTemplates[fileName] = filePath
+        return self._skeletonTemplates.keys()
 
     def GetSkeletonTemplatePath(self, name):
         return self._skeletonTemplates[name]
@@ -86,12 +104,157 @@ class File_Manager():
 
     def GetNextSkeletonBuildPath(self, name = ''):
         skelPath = os.path.join(self._buildFolderPath, 'Skeleton')
-        fileName = name + '_' + str(self._nextBuildVersion['Skeleton'])
-        # MISSING: account for if file exists...
+        fileName = '%s_%03d.json' %(name, self._nextBuildVersion['Skeleton'])
         self._nextBuildVersion['Skeleton'] += 1
+        if (os.path.isfile(fileName)):
+            fileName = self.GetNextSkeletonBuildPath(name)
         return os.path.join(skelPath, fileName)
+    
+    def IncrementSkeletonBuildIndex(self):
+        self._buildIndex['Skeleton'] += 1
 
-#========== SKELETONS ==================================
+    def DecrementSkeletonBuildIndex(self):
+        self._buildIndex['Skeleton'] -= 1
+    
+    def GetSkeletonBuildData(self):
+        if len(self._buildFileData['Skeleton']) == 0:
+            return None
+        return self._buildFileData['Skeleton'][self._buildIndex['Skeleton']]
+
+#========== BEHAVIORS ==================================
+
+    def GetBehaviorTemplates(self):
+        # Files NEED to exist
+        self._behaviorTemplates.clear()
+        bhvPath = os.path.join(self._templatePath, 'Behaviors')
+        for f in os.listdir(bhvPath):
+            filePath = os.path.join(bhvPath, f)
+            fileName, ext = os.path.splitext(f)
+            self._behaviorTemplates[fileName] = filePath
+        return self._behaviorTemplates.keys()
+
+    def GetBehaviorTemplatePath(self, name):
+        return self._behaviorTemplates[name]
+
+    def GetBehaviorCustomTemplates(self):
+        self._behaviorCustomTemplates.clear()
+        bhvPath = os.path.join(self._customTemplatePath, 'Behaviors')
+        if not os.path.isdir(bhvPath):
+            os.mkdir(bhvPath)
+        for f in os.listdir(bhvPath):
+            filePath = os.path.join(bhvPath, f)
+            fileName, ext = os.path.splitext(f)
+            self._behaviorCustomTemplates[fileName] = filePath
+
+    def GetBehaviorCustomTemplatePath(self, name=None):
+        '''Returns folder path if no name designated'''
+        if (name not in self._behaviorCustomTemplates.keys()):
+            return os.path.join(self._customTemplatePath, 'Behaviors')
+        return self._behaviorCustomTemplates[name]
+
+    def GetNextBehaviorBuildPath(self, name = ''):
+        bhvPath = os.path.join(self._buildFolderPath, 'Behaviors')
+        fileName = '%s_%03d.json' %(name, self._nextBuildVersion['Behaviors'])
+        self._nextBuildVersion['Behaviors'] += 1
+        if (os.path.isfile(fileName)):
+            fileName = self.GetNextBehaviorBuildPath(name)
+        return os.path.join(bhvPath, fileName)
+    
+    def IncrementBehaviorBuildIndex(self):
+        self._buildIndex['Behaviors'] += 1
+
+    def DecrementBehaviorBuildIndex(self):
+        self._buildIndex['Behaviors'] -= 1
+    
+    def GetBehaviorBuildData(self):
+        if len(self._buildFileData['Behaviors']) == 0:
+            return None
+        return self._buildFileData['Behaviors'][self._buildIndex['Behaviors']]
+
+#========== APPEARANCE ==================================
+
+    def GetAppearanceTemplates(self):
+        # Files NEED to exist
+        self._appearanceTemplates.clear()
+        appPath = os.path.join(self._templatePath, 'Appearance')
+        for f in os.listdir(appPath):
+            filePath = os.path.join(appPath, f)
+            fileName, ext = os.path.splitext(f)
+            self._appearanceTemplates[fileName] = filePath
+        return self._appearanceTemplates.keys()
+
+    def GetAppearanceTemplatePath(self, name):
+        return self._appearanceTemplates[name]
+
+    def GetAppearanceCustomTemplates(self):
+        self._appearanceCustomTemplates.clear()
+        appPath = os.path.join(self._customTemplatePath, 'Appearance')
+        if not os.path.isdir(appPath):
+            os.mkdir(appPath)
+        for f in os.listdir(appPath):
+            filePath = os.path.join(appPath, f)
+            fileName, ext = os.path.splitext(f)
+            self._appearanceCustomTemplates[fileName] = filePath
+
+    def GetAppearanceCustomTemplatePath(self, name=None):
+        '''Returns folder path if no name designated'''
+        if (name not in self._appearanceCustomTemplates.keys()):
+            return os.path.join(self._customTemplatePath, 'Appearance')
+        return self._appearanceCustomTemplates[name]
+
+    def GetNextAppearanceBuildPath(self, name = ''):
+        appPath = os.path.join(self._buildFolderPath, 'Appearance')
+        fileName = '%s_%03d.json' %(name, self._nextBuildVersion['Appearance'])
+        self._nextBuildVersion['Appearance'] += 1
+        if (os.path.isfile(fileName)):
+            fileName = self.GetNextAppearanceBuildPath(name)
+        return os.path.join(appPath, fileName)
+    
+    def IncrementAppearanceBuildIndex(self):
+        self._buildIndex['Appearance'] += 1
+
+    def DecrementAppearanceBuildIndex(self):
+        self._buildIndex['Appearance'] -= 1
+    
+    def GetAppearanceBuildData(self):
+        if len(self._buildFileData['Appearance']) == 0:
+            return None
+        return self._buildFileData['Appearance'][self._buildIndex['Appearance']]
+
+#========== SKINNING ==================================
+
+    def GetSkinningTemplates(self):
+        # Files NEED to exist
+        self._skinningTemplates.clear()
+        appPath = os.path.join(self._templatePath, 'Skinning')
+        for f in os.listdir(appPath):
+            filePath = os.path.join(appPath, f)
+            fileName, ext = os.path.splitext(f)
+            self._skinningTemplates[fileName] = filePath
+        return self._skinningTemplates.keys()
+
+    def GetSkinningTemplatePath(self, name):
+        return self._skinningTemplates[name]
+
+    def GetNextSkinningBuildPath(self, name = ''):
+        appPath = os.path.join(self._buildFolderPath, 'Skinning')
+        fileName = '%s_%03d.json' %(name, self._nextBuildVersion['Skinning'])
+        self._nextBuildVersion['Skinning'] += 1
+        if (os.path.isfile(fileName)):
+            fileName = self.GetNextSkinningBuildPath(name)
+        return os.path.join(appPath, fileName)
+    
+    def IncrementSkinningBuildIndex(self):
+        self._buildIndex['Skinning'] += 1
+
+    def DecrementSkinningBuildIndex(self):
+        self._buildIndex['Skinning'] -= 1
+    
+    def GetSkinningBuildData(self):
+        if len(self._buildFileData['Skinning']) == 0:
+            return None
+        return self._buildFileData['Skinning'][self._buildIndex['Skinning']]
+
 
 
 class File_Data():
--- RigSetup/_Tests.py
+++ RigSetup/_Tests.py
@@ -54,7 +54,8 @@ class Test_Name_Manager(unittest.TestCase):
                                     objType='JNT')
         self.assertTrue(name == 'CAT_L_ELBOW_ARM_01_JNT')
 
-class Test_File_Manager(unittest.TestCase):
+@unittest.skip("skipping...")
+class Test_File_Manager_Skeleton(unittest.TestCase):
     def setUp(self):
         self.fileMng = File_Manager(r'D:\Assets\Programming\Python\Maya\ModularAutoRigger\Templates')
         self.fileMng.SetBuildFolderPath(r'C:\Users\crash\Documents\TPRAS_BuildFiles')
@@ -63,8 +64,8 @@ class Test_File_Manager(unittest.TestCase):
     def tearDown(self):
         del(self.fileMng)
 
-    def test_GetBuildFilesInfo(self):
-        self.fileMng.GetBuildFilesInfo()
+    def test_InitBuildFilesInfo(self):
+        self.fileMng.InitBuildFilesInfo()
         self.assertTrue(self.fileMng._buildPaths['Appearance'][0].path)
         self.assertTrue(self.fileMng._buildPaths['Appearance'][0].version)
         self.assertTrue(self.fileMng._buildPaths['Appearance'][0].date)
@@ -84,8 +85,123 @@ class Test_File_Manager(unittest.TestCase):
         path = self.fileMng.GetSkeletonCustomTemplatePath('Custom_Arm_Biped')
         self.assertTrue(os.path.isfile(path))
 
+    def test_GetNextSkeletonBuildPath(self):
+        self.fileMng.InitBuildFilesInfo()
+        path = self.fileMng.GetNextSkeletonBuildPath('asdf')
+        # print(path)
+        self.assertFalse(os.path.isfile(path))
+
+    def test_GetSkeletonBuildData(self):
+        self.fileMng.InitBuildFilesInfo()
+        data = self.fileMng.GetSkeletonBuildData()
+        print(data.path)
+
+@unittest.skip("skipping...")
+class Test_File_Manager_Behaviors(unittest.TestCase):
+    def setUp(self):
+        self.fileMng = File_Manager(r'D:\Assets\Programming\Python\Maya\ModularAutoRigger\Templates')
+        self.fileMng.SetBuildFolderPath(r'C:\Users\crash\Documents\TPRAS_BuildFiles')
+        self.fileMng.SetCustomTemplatePath(r'C:\Users\crash\Documents\TPRAS_CustomTemplates')
+        
+    def tearDown(self):
+        del(self.fileMng)
+
+    def test_GetBehaviorTemplates(self):
+        self.fileMng.GetBehaviorTemplates()
+        # print (self.fileMng._behaviorTemplates.keys())
+        self.assertTrue('Arm_Biped' in self.fileMng._behaviorTemplates.keys())
+
+    def test_GetBehaviorTemplatePath(self):
+        self.fileMng.GetBehaviorTemplates()
+        path = self.fileMng.GetBehaviorTemplatePath('Arm_Biped')
+        self.assertTrue(os.path.isfile(path))
+    
+    def test_GetBehaviorCustomTemplates(self):
+        self.fileMng.GetBehaviorCustomTemplates()
+        path = self.fileMng.GetBehaviorCustomTemplatePath('Custom_Arm_Biped')
+        self.assertTrue(os.path.isfile(path))
+
+    def test_GetNextBehaviorBuildPath(self):
+        self.fileMng.InitBuildFilesInfo()
+        path = self.fileMng.GetNextBehaviorBuildPath('asdf')
+        # print(path)
+        self.assertFalse(os.path.isfile(path))
+
+    def test_GetBehaviorBuildData(self):
+        self.fileMng.InitBuildFilesInfo()
+        data = self.fileMng.GetBehaviorBuildData()
+        if (data):
+            print(data.path)
 
 
+@unittest.skip("skipping...")
+class Test_File_Manager_Appearance(unittest.TestCase):
+    def setUp(self):
+        self.fileMng = File_Manager(r'D:\Assets\Programming\Python\Maya\ModularAutoRigger\Templates')
+        self.fileMng.SetBuildFolderPath(r'C:\Users\crash\Documents\TPRAS_BuildFiles')
+        self.fileMng.SetCustomTemplatePath(r'C:\Users\crash\Documents\TPRAS_CustomTemplates')
+        
+    def tearDown(self):
+        del(self.fileMng)
+
+    def test_GetAppearanceTemplates(self):
+        self.fileMng.GetAppearanceTemplates()
+        # print (self.fileMng._appearanceTemplates.keys())
+        self.assertTrue('Arm_Biped' in self.fileMng._appearanceTemplates.keys())
+
+    def test_GetAppearanceTemplatePath(self):
+        self.fileMng.GetAppearanceTemplates()
+        path = self.fileMng.GetAppearanceTemplatePath('Arm_Biped')
+        self.assertTrue(os.path.isfile(path))
+    
+    def test_GetAppearanceCustomTemplates(self):
+        self.fileMng.GetAppearanceCustomTemplates()
+        path = self.fileMng.GetAppearanceCustomTemplatePath('Custom_Arm_Biped')
+        self.assertTrue(os.path.isfile(path))
+
+    def test_GetNextAppearanceBuildPath(self):
+        self.fileMng.InitBuildFilesInfo()
+        path = self.fileMng.GetNextAppearanceBuildPath('asdf')
+        # print(path)
+        self.assertFalse(os.path.isfile(path))
+
+    def test_GetAppearanceBuildData(self):
+        self.fileMng.InitBuildFilesInfo()
+        data = self.fileMng.GetAppearanceBuildData()
+        if (data):
+            print(data.path)
+
+
+class Test_File_Manager_Skinning(unittest.TestCase):
+    def setUp(self):
+        self.fileMng = File_Manager(r'D:\Assets\Programming\Python\Maya\ModularAutoRigger\Templates')
+        self.fileMng.SetBuildFolderPath(r'C:\Users\crash\Documents\TPRAS_BuildFiles')
+        self.fileMng.SetCustomTemplatePath(r'C:\Users\crash\Documents\TPRAS_CustomTemplates')
+        
+    def tearDown(self):
+        del(self.fileMng)
+
+    def test_GetSkinningTemplates(self):
+        self.fileMng.GetSkinningTemplates()
+        # print (self.fileMng._skinningTemplates.keys())
+        self.assertTrue('Arm_Biped' in self.fileMng._skinningTemplates.keys())
+
+    def test_GetSkinningTemplatePath(self):
+        self.fileMng.GetSkinningTemplates()
+        path = self.fileMng.GetSkinningTemplatePath('Arm_Biped')
+        self.assertTrue(os.path.isfile(path))
+
+    def test_GetNextSkinningBuildPath(self):
+        self.fileMng.InitBuildFilesInfo()
+        path = self.fileMng.GetNextSkinningBuildPath('asdf')
+        # print(path)
+        self.assertFalse(os.path.isfile(path))
+
+    def test_GetSkinningBuildData(self):
+        self.fileMng.InitBuildFilesInfo()
+        data = self.fileMng.GetSkinningBuildData()
+        if (data):
+            print(data.path)
 
 
 
